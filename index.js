9a90a3367fdd1e051e984ea4c29164715f01aac2
Emma Goodman <emmagoodmannyc@gmail.com>
2018-03-18 23:04:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+//describe('shout(string)', function() {
+ // it('receives one argument and returns it in all caps'//, function() {
+   // expect(shout('hello')).toEqual('HELLO')
+ // })
+//})
+
+function shout(string){
+  return string.toUpperCase()
+}
