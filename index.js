e12185edc3d03f5ebe4f60fa1b28271e4814bc8e
Emma Goodman <emmagoodmannyc@gmail.com>
2018-03-18 23:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function shout(string) {
 function whisper(STRING) {
   return string.toLowerCase()
 }
+
+//describe('logShout(string)', function() {
+//  it('calls console.log() its one argument in all //caps', function() {
+ //   const spy = expect.spyOn(console, 'log'//).andCallThrough()
+
+ //   logShout('hello')
+
+ //   expect(spy).toHaveBeenCalledWith('HELLO')
+
+   // console.log.restore()
+ // })
+// })
+
+function shout(string) {
+  console.log(string.toUpperCase())
+}
+
